90f6a25b2bfa0786c199c2ca77675a989f38edce
Manuel Torres <mn.torres1993@gmail.com>
2023-09-27 14:48:02 -0300

    cambios en proceso

--- app/page.js
+++ app/page.js
@@ -1,14 +1,16 @@
-'use client';
+"use client";
 import React from "react";
 import TableEvents from "./components/TableEvents";
 
 const Home = () => {
   return (
-    <div className="container mx-auto p-4">
-      <h1 className="text-2x1 font-semibold mb-4">Sistema De Eventualidades</h1>
+    <div className="w-screen h-screen">
+      <h1 className="text-2x1 font-semibold mb-4 m-5">
+        Sistema De Eventualidades
+      </h1>
       <TableEvents />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
