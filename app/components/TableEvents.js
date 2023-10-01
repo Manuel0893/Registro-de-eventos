bb13ddc35aee601453fced8cf2c63e1b0c607b43
Manuel Torres <mn.torres1993@gmail.com>
2023-10-01 20:36:17 -0300

    mejoras en proceso

--- app/components/TableEvents.js
+++ app/components/TableEvents.js
@@ -74,29 +74,29 @@ export default function TableEvents() {
   };
 
   return (
-    <div className="m-5 ml-48">
+    <div className="m-1 ml-40">
       <h2 className="text-xl font-semibold mb-4">
         Registros de Eventualidades
       </h2>
-      <div className="mb-4">
+      <div className="mb-4 ">
         <input
           type="text"
           placeholder="Nombre"
-          className="p-2 border mx-3 text-gray-900"
+          className="p-2 rounded mx-3 text-gray-900"
           value={nombre}
           onChange={(e) => setNombre(e.target.value)}
         />
         <input
           type="text"
           placeholder="Apellido"
-          className="p-2 border mx-3 text-gray-900"
+          className="p-2 rounded mx-3 text-gray-900"
           value={apellido}
           onChange={(e) => setApellido(e.target.value)}
         />
         <input
           type="text"
           placeholder="Curso"
-          className="p-2 border mx-3 text-gray-900"
+          className="p-2 rounded  mx-3 text-gray-900"
           value={curso}
           onChange={(e) => setCurso(e.target.value)}
         />
@@ -107,35 +107,41 @@ export default function TableEvents() {
           Agregar Alumno
         </button>
       </div>
-      <table className="table-auto w-4/5">
+      <table className="border-separate rounded border border-slate-500 w-4/5">
         <thead>
           <tr>
-            <th className="border px-4 py-2">Nombre</th>
-            <th className="border px-4 py-2">Apellidos</th>
-            <th className="border px-4 py-2">Curso</th>
-            <th className="border px-4 py-2">Eventualidades</th>
-            <th className="border px-4 py-2">Acciones</th>
+            <th className="border rounded border-slate-300 py-2 ...">Nombre</th>
+            <th className="border rounded border-slate-300 ...">Apellidos</th>
+            <th className="border rounded border-slate-300 ...">Curso</th>
+            <th className="border rounded border-slate-300 ...">Eventualidades</th>
+            <th className="border rounded border-slate-300 ...">Acciones</th>
           </tr>
         </thead>
         <tbody>
           {alumnos.map((alumno, index) => (
             <tr key={index} className="border">
-              <td className="border px-4 py-2">{alumno.name}</td>
-              <td className="border px-4 py-2">{alumno.apellido}</td>
-              <td className="border px-4 py-2">{alumno.curso}</td>
-              <td className="border px-4 py-2">{alumno.events}</td>
-              <td className="border px-4 py-2">
+              <td className="border rounded border-slate-700 px-4 py-2 text-center">{alumno.name}</td>
+              <td className="border rounded border-slate-700 px-4 py-2 text-center ... ">{alumno.apellido}</td>
+              <td className="border rounded border-slate-700 px-4 py-2 text-center ... ">{alumno.curso}</td>
+              <td className="border rounded border-slate-700 px-4 py-2 text-center ...">{alumno.events}</td>
+              <td className="border rounded border-slate-700 px-4 py-2 flex items-center justify-center">
                 <button
-                  className="text-red-600 hover:underline"
+                  className="bg-red-600 px-4 py-1 rounded hover:underline flex items-center mr-2"
                   onClick={() => handleDeleteAlumno(index)}
                 >
                   Eliminar
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+</svg>
                 </button>
                 <button
-                  className="ml-2 text-blue-600 hover:underline"
-                  onClick={() => handleOpenModal(alumno)}
+                  className="bg-green-600 rounded px-4 py-1 text-white hover:underline flex items-center ml-2"
+                  onClick={() => handleOpenModal(alumno)} 
                 >
                   Ver / Agregar Eventualidad
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke-width="1.5" stroke="currentColor" className="w-6 h-6 ml-4">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+                      </svg>
                 </button>
               </td>
             </tr>
@@ -160,9 +166,9 @@ export default function TableEvents() {
                         Fecha: {evento.fecha}
                       </p>
                     </div>
-                  </div>
-                ))}
-              </div>
+                  </div>                          
+              ))}
+            </div>
             </div>
             <textarea
               type="text"
